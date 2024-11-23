32a35cc22e30b5dbd7510e42a40fff761fbf73ac
Lam Nguyen <lamnguyen11e@gmail.com>
2024-11-23 16:29:06 +0700

    feat(theme): add default variable tokens for theme css

---
+++ src/settings/config.ts
@@ -0,0 +1,14 @@
+const style = {
+  theme: "dark", // dark | light
+  neutral: "gray", // sand | gray | slate
+  brand: "cyan", // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
+  accent: "indigo", // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
+  solid: "contrast", // color | contrast
+  solidStyle: "flat", // flat | plastic
+  border: "playful", // rounded | playful | conservative
+  surface: "translucent", // filled | translucent
+  transition: "all", // all | micro | macro
+  scaling: "100", // 90 | 95 | 100 | 105 | 110
+};
+
+export { style };
