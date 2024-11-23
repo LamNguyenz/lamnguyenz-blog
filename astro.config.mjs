6c1a01e4f4ec4eb41d238735f91ea4778b795033
Lam Nguyen <lamnguyen11e@gmail.com>
2024-11-23 16:49:16 +0700

    ci: add yml config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import tailwind from '@tailwindcss/vite';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
-    integrations: [mdx(), sitemap()],
-    vite: {
-        plugins: [tailwind()],
-    },
+  site: "https://lamnguyenz.github.io",
+  base: "blog",
+  integrations: [mdx(), sitemap()],
+  vite: {
+    plugins: [tailwind()],
+  },
 });
